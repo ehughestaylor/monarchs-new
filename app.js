fbdf0817771b619cd60587a04ae88fe67822f0cc
Elliott Hughes-Taylor <ehughestaylor@hotmail.com>
2015-02-14 18:36:31 -0500

    bug

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
-var port = process.eng.PORT || 8000;
+var port = process.env.PORT || 8000;
+
 app.listen(port);
